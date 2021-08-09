0f765c945e4f7906b1f06f3065f212d2e65550e3
vladimir klonin <vklonin@gmail.com>
2021-08-09 13:07:56 +0300

    added allure lambda steps

--- src/test/java/tests/StudentRegistrationTest.java
+++ src/test/java/tests/StudentRegistrationTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import utils.TestData;
+import static io.qameta.allure.Allure.step;
 
 public class StudentRegistrationTest {
 
@@ -19,23 +20,23 @@ public class StudentRegistrationTest {
 
         TestData testData = new TestData();
 
-        studentRegPage.openPage();
-        studentRegPage.inputFirstName(testData.firstName);
-        studentRegPage.inputLastName(testData.lastName);
-        studentRegPage.inputEMail(testData.emailAddress);
-        studentRegPage.inputGender(testData.gender);
-        studentRegPage.inputMPhone(testData.cellPhone);
-        studentRegPage.inputBirthDate(testData.dateOfBirth);
-        studentRegPage.inputSubject(testData.subjects[0]);
-        studentRegPage.inputSubject(testData.subjects[1]);
-        studentRegPage.inputHobby(testData.hobbys[0]);
-        studentRegPage.inputHobby(testData.hobbys[1]);
-        studentRegPage.uploadPicture(testData.picPath);
-        studentRegPage.inputAddress(testData.currentAddress);
-        studentRegPage.inputState(testData.state);
-        studentRegPage.inputCity(testData.city);
-        studentRegPage.clickSubmit();
-        studentRegPage.checkOutcome(testData);
+        step("Open main page" , studentRegPage::openPage);
+        step("Input first name" , () -> studentRegPage.inputFirstName(testData.firstName));
+        step("Input last name" , () -> studentRegPage.inputLastName(testData.lastName));
+        step("Input email" , () -> studentRegPage.inputEMail(testData.emailAddress));
+        step("Input gender" , () -> studentRegPage.inputGender(testData.gender));
+        step("Input cell phone" , () -> studentRegPage.inputMPhone(testData.cellPhone));
+        step("Input date of birth" , () -> studentRegPage.inputBirthDate(testData.dateOfBirth));
+        step("Input subject 1" , () -> studentRegPage.inputSubject(testData.subjects[0]));
+        step("Input subject 2" , () -> studentRegPage.inputSubject(testData.subjects[1]));
+        step("Input hobby 1" , () -> studentRegPage.inputHobby(testData.hobbys[0]));
+        step("Input hobby 2" , () -> studentRegPage.inputHobby(testData.hobbys[1]));
+        step("Input picture" , () -> studentRegPage.uploadPicture(testData.picPath));
+        step("Input address" , () -> studentRegPage.inputAddress(testData.currentAddress));
+        step("Input state" , () -> studentRegPage.inputState(testData.state));
+        step("Input city" , () -> studentRegPage.inputCity(testData.city));
+        step("Submit form" , studentRegPage::clickSubmit);
+        step("Check results" , () -> studentRegPage.checkOutcome(testData));
 
 
     }
